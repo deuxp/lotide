fe19f2d15a5027bf7f9313df0b6c176deb5a3db7
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-20 03:17:41 +0000

    created custom assertion tester

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+/* custom assert function which will log a message to console */
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log('🚨🚨🚨 Assertion Failed: ', actual, ' !== ', expected);
+  } else {
+    console.log('💎💎💎 Assertion Passed: ', actual, ' === ', expected);
+
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 1.1);
+assertEqual(1, '1');
