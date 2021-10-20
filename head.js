1362a43b1f8b9345e6740be51e88ce7295a64568
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-20 03:53:24 +0000

    created the head.js addition to the library

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+const head = array => {
+  return array.splice(0, 1)[0];
+};
+
+
+assertEqual(head([5, 6, 7]), 5); // Pass
+assertEqual(head([5]), 5); // Pass
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); // Pass
+assertEqual(head([]), undefined); // Pass
