57f40fb1c5d9387c96ab3de8c1788672f99e759b
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-21 20:50:10 +0000

    improve human readability

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,14 @@
+const isString = input => {
+  return typeof input === "string";
+};
+
+
 /* custom assert function which will log a message to console */
 const assertEqual = (actual, expected) => {
   if (actual !== expected) {
-    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🚨🚨🚨 Assertion Failed: ${isString(actual) ? '"' + actual + '"' : actual} !== ${isString(expected) ? '"' + expected + '"' : expected}`);
   } else {
-    console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`💎💎💎 Assertion Passed: ${isString(actual) ? '"' + actual + '"' : actual} === ${isString(expected) ? '"' + expected + '"' : expected}`);
   }
 };
 
@@ -13,3 +18,8 @@ assertEqual("Bootcamp", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, 1.1);
 assertEqual(1, '1');
+
+// String.isString(actual) ? '"'+actual+'"' : actual
+// console.log(isString('hello'));
+// console.log(isString(5));
+// console.log(isString(['hello']));
