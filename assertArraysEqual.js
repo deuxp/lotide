1e5be1b673d20e893ea9fb4c0d917359c9627e59
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-25 04:00:16 +0000

    improvements on test assertions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,33 @@
 
 // array equality
-const assertArraysEqual = (array1, array2) => {
-  let equality = 0;
-
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        equality++;
-      }
-    }
-  }
-  if (equality * 2 === array1.length + array2.length) {
-    console.log(`💎💎💎 Assertion Passed: ${array1} === ${array2}`);
+const assertArraysEqual = (actual, expected) => {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🚨🚨🚨 Assertion Failed: ${array1} !== ${array2}`);
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 
-let a = [undefined]; let b = []
 
-assertArraysEqual(a, b)
-console.log(a);
+//   let equality = 0;
+
+//   if (array1.length === array2.length) {
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array1[i] === array2[i]) {
+//         equality++;
+//       }
+//     }
+//   if (equality * 2 === array1.length + array2.length) {
+//   }
+//     console.log(`💎💎💎 Assertion Passed: ${array1} === ${array2}`);
+//   } else {
+//     console.log(`🚨🚨🚨 Assertion Failed: ${array1} !== ${array2}`);
+//   }
+// };
+
+
+// let a = [undefined]; let b = []
+
+// assertArraysEqual(a, b)
+// console.log(a);
