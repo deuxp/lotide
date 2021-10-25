6d491de60e5e8e1626b7f86a020de64c2953830d
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-25 14:54:33 +0000

    passed further testing

--- middle.js
+++ middle.js
@@ -15,12 +15,14 @@ const middle = array => {
 };
 
 
-let a = [1,2,3,4,5,6]; // 3,4
-let b = [1,2,3,4,5]; // 3
-let c = [1,2,3,4,5,6,7,8,9]; // 5
-let d = []; // []
+let a = [1,2,3,4,5,6]; // 3,4     pass
+let b = [1,2,3,4,5]; // 3         pass
+let c = [1,2,3,4,5,6,7,8,9]; // 5 pass
+let d = []; // []                 pass
+let z = [1,2,3,'4',5,6]; // 3,'4' fail
 
 assertArraysEqual(middle(a), [3, 4])
 assertArraysEqual(middle(b), [3])
 assertArraysEqual(middle(c), [5])
 assertArraysEqual(middle(d), [])
+assertArraysEqual(middle(z), [3, 4])
