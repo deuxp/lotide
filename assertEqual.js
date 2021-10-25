4cb90a1002309e216e9185d0be14433f18bcc8d6
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-25 23:39:35 +0000

    improvements to the compactness

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,6 @@
-const isString = input => {
-  return typeof input === "string";
-};
-
 
 /* custom assert function which will log a message to console */
+// paired with assertEqual: dependency
 const assertEqual = (actual, expected) => {
   if (actual !== expected) {
     console.log(`🚨🚨🚨 Assertion Failed: ${isString(actual) ? '"' + actual + '"' : actual} !== ${isString(expected) ? '"' + expected + '"' : expected}`);
