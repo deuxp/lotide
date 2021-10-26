b8d9493a99c035dc69aac936b280c7bc5c54f2a4
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-26 03:18:31 +0000

    added a function that compares 2 objects for "==="

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,28 @@
+// ========================================================================
+// helper functions
+// ========================================================================
+
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+const assertEqual = (actual, expected) => {
+  const isString = input => typeof input === "string";
+  if (actual !== expected) {
+    console.log(`🚨🚨🚨 Assertion Failed: ${isString(actual) ? '"' + actual + '"' : actual} !== ${isString(expected) ? '"' + expected + '"' : expected}`);
+  } else {
+    console.log(`💎💎💎 Assertion Passed: ${isString(actual) ? '"' + actual + '"' : actual} === ${isString(expected) ? '"' + expected + '"' : expected}`);
+  }
+};
+
+
+// ========================================================================
+// the function: eqObjects()
+// ========================================================================
 /*
 input -> object actual, object expected
 paramPrep: compare four lists: keys for both and values for both
@@ -10,7 +35,7 @@ return make it to the end no error TRUE
 output-> Boolean
 
 */
-const eqObject = (actual, expected) => {
+const eqObjects = (actual, expected) => {
   const actualKeys = Object.keys(actual);
   const expectedKeys = Object.keys(expected);
 
@@ -18,22 +43,25 @@ const eqObject = (actual, expected) => {
 
   for (let i = 0; i < actualKeys.length; i++) {
     const key = actualKeys[i];
-    if (actual[key] !== expected[key]) return false;
+
+    if (actual[key] !== expected[key]) {
+      if (Array.isArray(actual[key]) && Array.isArray(expected[key])) {
+        if (eqArrays(actual[key], expected[key])) {
+          continue;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
   }
   return true;
 };
 
-
-const assertEqual = (actual, expected) => {
-  const isString = input => typeof input === "string";
-  if (actual !== expected) {
-    console.log(`🚨🚨🚨 Assertion Failed: ${isString(actual) ? '"' + actual + '"' : actual} !== ${isString(expected) ? '"' + expected + '"' : expected}`);
-  } else {
-    console.log(`💎💎💎 Assertion Passed: ${isString(actual) ? '"' + actual + '"' : actual} === ${isString(expected) ? '"' + expected + '"' : expected}`);
-  }
-};
-
-
+// ========================================================================
+// tests
+// ========================================================================
 let a1 = {a: 1, b: 2, c: 3};
 let a2 = {a: 1, c: 3, b: 2};
 let a3 = {a: 1, b: 2, c: '3'}; // fail
@@ -47,9 +75,17 @@ let b4 = {a: undefined};
 let b5 = {a: 0};
 let b6 = {a: null};
 
-assertEqual(eqObject(a1, a2), true);
-assertEqual(eqObject(a1, a3), false);
-assertEqual(eqObject(a1, a4), false);
-assertEqual(eqObject(b1, b2), true);
-assertEqual(eqObject(b3, b4), false);
-assertEqual(eqObject(b5, b6), false);
+assertEqual(eqObjects(a1, a2), true);
+assertEqual(eqObjects(a1, a3), false);
+assertEqual(eqObjects(a1, a4), false);
+assertEqual(eqObjects(b1, b2), true);
+assertEqual(eqObjects(b3, b4), false);
+assertEqual(eqObjects(b5, b6), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+
