12eb189088bfd49dda78fdd6152374a21ce8f920
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-27 00:34:39 +0000

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,57 @@
+// ==================================
+// helper function
+// ==================================
 
-// array equality
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+// ==================================
+// IMPLEMENTATION
+// ==================================
+
 const assertArraysEqual = (actual, expected) => {
-  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+  if (eqArrays(actual, expected)) {
     console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-
-
-//   let equality = 0;
-
-//   if (array1.length === array2.length) {
-//     for (let i = 0; i < array1.length; i++) {
-//       if (array1[i] === array2[i]) {
-//         equality++;
-//       }
-//     }
-//   if (equality * 2 === array1.length + array2.length) {
-//   }
-//     console.log(`💎💎💎 Assertion Passed: ${array1} === ${array2}`);
+// ==================================
+// alternates 1
+// ==================================
+// const assertArraysEqual = (actual, expected) => {
+//   if (JSON.stringify(actual) === JSON.stringify(expected)) {
+//     console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
 //   } else {
-//     console.log(`🚨🚨🚨 Assertion Failed: ${array1} !== ${array2}`);
+//     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
 //   }
 // };
 
 
-// let a = [undefined]; let b = []
+// ==================================
+// alternates 2
+// ==================================
+// const assertArraysEqual = (actual, expected) => {
+//   let assertion = true;
+//   if (actual.length !== expected.length) assertion = false;
+//   for (let i = 0; i < actual.length; i++) {
+//     if (actual[i] !== expected[i]) assertion = false;
+//   }
+//   if (assertion) {
+//     console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
-// assertArraysEqual(a, b)
-// console.log(a);
+
+
+let a = [5, 6, 8, 9]; let b = [5, '6', 8, 9];
+
+assertArraysEqual(a, b);
