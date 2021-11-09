19f4538ed3da789fc18b812470196af4da51f1d6
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-08 21:24:00 -0500

    refactor and export assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,57 +1,14 @@
-// ==================================
-// helper function
-// ==================================
-
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArray')
 
 // ==================================
-// IMPLEMENTATION
+// Compare two arrays for equality
 // ==================================
 
 const assertArraysEqual = (actual, expected) => {
-  if (eqArrays(actual, expected)) {
-    console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-  }
+  const pass = `💎💎💎 Assertion Passed: ${actual} === ${expected}`;
+  const notPass = `👁 👄 👁 Assertion Failed: ${actual} !== ${expected}`;
+  eqArrays(actual, expected) ? console.log(pass) : console.log(notPass);
 };
 
-// ==================================
-// alternates 1
-// ==================================
-// const assertArraysEqual = (actual, expected) => {
-//   if (JSON.stringify(actual) === JSON.stringify(expected)) {
-//     console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
 
-
-// ==================================
-// alternates 2
-// ==================================
-// const assertArraysEqual = (actual, expected) => {
-//   let assertion = true;
-//   if (actual.length !== expected.length) assertion = false;
-//   for (let i = 0; i < actual.length; i++) {
-//     if (actual[i] !== expected[i]) assertion = false;
-//   }
-//   if (assertion) {
-//     console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-
-
-let a = [5, 6, 8, 9]; let b = [5, '6', 8, 9];
-
-assertArraysEqual(a, b);
+module.exports = assertArraysEqual;
