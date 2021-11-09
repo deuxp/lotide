f53be01e10899c073c69244ca560f4491d1058af
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-09 01:08:19 -0500

    refactored to use mocha and chai library for testing

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,13 +1,28 @@
-const assertEqual = require('../assertEqual')
 const tail = require('../tail')
+const assert = require('chai').assert;
 
-// assertEqual(actual, expected)
-// assertEqual(actual, expected)
-// assertEqual(actual, expected)
-// assertEqual(actual, expected)
-// assertEqual(actual, expected)
-assertEqual(tail([1,2,3,4])[0], 2)
-assertEqual(tail([1,2,3,4])[1], 3)
-assertEqual(tail([1,2,3,4])[2], 4)
-assertEqual(tail([1,2,3,4])[3], undefined)
-assertEqual(tail([])[0], [][0])
+describe('#tail', () => {
+  it('returns[2,3,4], from [1,2,3,4]', () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4])
+  })
+
+  it('returns [] from []', () => {
+    assert.deepEqual(tail([]), [])
+  })
+
+  it('returns [] from [8]', () => {
+    assert.deepEqual(tail([8]), [])
+  })
+
+  it('returns [] from "hello there"', () => {
+    assert.deepEqual(tail('hello there'), [])
+  })
+})
+
+
+
+// assertEqual(tail()[0], 2)
+// assertEqual(tail([1,2,3,4])[1], 3)
+// assertEqual(tail([1,2,3,4])[2], 4)
+// assertEqual(tail([1,2,3,4])[3], undefined)
+// assertEqual(tail([])[0], [][0])
